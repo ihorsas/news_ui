9109a98e934e78528447578beccf91a6b32b498e
isas <isas@roku.com>
2022-05-16 20:15:51 +0300

    add UI for news

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {gql, useQuery} from "@apollo/client";
+
+
+const NEWS = gql`
+  query news {
+  news {
+    id
+    title
+    author
+    views
+    publishDate
+  }
+}
+`;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h2>All scrapped news</h2>
+            <ul>
+                <News/>
+            </ul>
+        </div>
+    );
+}
+
+function News() {
+    const {loading, error, data} = useQuery(NEWS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return data.news.map(({id, title, author, views, publishDate}) => (
+        <li key={id}>
+            <div>Title: {title}</div>
+            <div>Author: {author}</div>
+            <div>Views: {views}</div>
+            <div>Publish date: {publishDate}</div>
+        </li>
+    ));
 }
 
 export default App;
